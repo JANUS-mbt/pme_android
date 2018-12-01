4e07d0cfdd278ca05180a4d7b66875506b5edc0e
ahmetcj4 <ahmetcj4@hotmail.com>
2018-12-01 23:08:22 +0300

    add fake loading on main screen

--- app/src/main/java/com/janus/platoon/vm/MainVM.kt
+++ app/src/main/java/com/janus/platoon/vm/MainVM.kt
@@ -3,13 +3,21 @@ package com.janus.platoon.vm
 import android.view.View
 import com.janus.platoon.base.BaseViewModel
 import com.janus.platoon.ui.choose.ChooseActivity
+import com.janus.platoon.util.Status
 import com.janus.platoon.util.startActivityWithName
+import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 
 class MainVM
 @Inject constructor() : BaseViewModel() {
     fun performPlatoonMe(view: View) {
-        view.context.startActivityWithName(ChooseActivity::class.java, false)
+        status.postValue(Status.IN_PROGRESS)
+        //todo replace it with request
+        add(Observable.timer(2, TimeUnit.SECONDS).subscribe {
+            view.context.startActivityWithName(ChooseActivity::class.java, false)
+            status.postValue(Status.NOT_STARTED)
+        })
     }
 }

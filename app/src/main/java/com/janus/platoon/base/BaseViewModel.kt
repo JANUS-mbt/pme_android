45a8b5d5f3dd5b322f66cdd1e1bb435e6e35c86f
ahmetcj4 <ahmetcj4@hotmail.com>
2018-12-01 22:44:08 +0300

    add status field to base vm to observe network requests

--- app/src/main/java/com/janus/platoon/base/BaseViewModel.kt
+++ app/src/main/java/com/janus/platoon/base/BaseViewModel.kt
@@ -1,13 +1,19 @@
 package com.janus.platoon.base
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import com.janus.platoon.util.Status
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
 open class BaseViewModel : ViewModel() {
     val TAG = javaClass.simpleName
-
     private val disposables = CompositeDisposable()
+    val status = MutableLiveData<Status>()
+
+    init {
+        status.value = Status.NOT_STARTED
+    }
 
     fun add(d: Disposable) {
         disposables.add(d)
